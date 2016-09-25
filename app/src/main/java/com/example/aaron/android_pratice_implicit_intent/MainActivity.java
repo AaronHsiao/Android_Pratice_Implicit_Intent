cff2ee54c3c09448ac72ffaf1d1bba3946501c14
Aaron <chihyenhsiao>
2016-09-25 16:13:29 +0800

    Explicit Intent (明確的意圖) 使用於同一應用程式中，不同Activity的傳遞

--- app/src/main/java/com/example/aaron/android_pratice_implicit_intent/MainActivity.java
+++ app/src/main/java/com/example/aaron/android_pratice_implicit_intent/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.aaron.android_pratice_implicit_intent;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(listener);
     }
+
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent();
+            intent.setClass(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }
+    };
 }
